38dc07797381a17f3f7430b8082f87910f9dc7b8
Faraz <farazsahmad8502@gmail.com>
2026-06-23 20:35:18 +0530

    refactor(app): set up dark dashboard shell layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="min-h-screen bg-zinc-950 text-white">
+    <div className="bg-background text-on-surface font-body-md">
       <Navbar />
 
-      <div className="flex">
+      <div className="flex min-h-screen">
         <Sidebar />
 
-        <main className="flex-1">
-          <Home />
+        <main className="flex-1 lg:ml-64 p-margin-mobile lg:p-margin-desktop pb-32 lg:pb-margin-desktop">
+          <div className="max-w-container-max mx-auto">
+            <Home />
+          </div>
         </main>
       </div>
     </div>
